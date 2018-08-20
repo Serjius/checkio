2a544316dc9790497031ab81da60610df6bd9dec
Serge Puchnin <Sergey.Puchnin@gmail.com>
2018-08-20 22:27:26 +0300

    Solved

---
+++ flatten-list.py
@@ -0,0 +1,21 @@
+import functools
+
+
+def flat_list(array):
+    lst = []
+    while array:
+        sublist = array.pop(0)
+        if isinstance(sublist, list):
+            array = sublist + array
+        else:
+            lst.append(sublist)
+    return(lst)
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [
+        2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
