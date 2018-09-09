70411f2698bddeed0674f28bd85d64c16fbab8a4
Serge Puchnin <Sergey.Puchnin@gmail.com>
2018-09-09 23:57:41 +0300

    Solved

--- find-enemy.py
+++ find-enemy.py
@@ -3,8 +3,8 @@ def hex_dist(sp, ep):
     your_x = sp[0]
     your_y = int(sp[1])
     dist_dic = {}
-    max_n = 5
-    n = 4
+    max_n = 20
+    n = 1
     while n <= max_n:
         dist_dic[n] = set()
         n1 = your_x
@@ -20,7 +20,7 @@ def hex_dist(sp, ep):
 
         # upper diagonal
         sh = n
-        if (ord(your_x) - ord('A')) % 2 != 0:
+        if n % 2 != 0 and (ord(your_x) - ord('A')) % 2 != 0:
             sh -= 1
         for i in range(1, n):
             n1 = chr(ord(your_x) - i)
@@ -32,7 +32,7 @@ def hex_dist(sp, ep):
             n2 = str(your_y - sh)
             if n1 >= 'A' and n1 <= 'Z' and n2 >= '1' and n2 <= '9':
                 dist_dic[n].add(n1 + n2)
-                #print('1-1:', n, (i, -sh), n1 + n2)
+                #print('1-2:', n, (i, -sh), n1 + n2)
             sh -= 1
 
         # bottom diagonal
@@ -75,6 +75,7 @@ def hex_dist(sp, ep):
     # D3
     # k1 = ['D2', 'C3', 'C4', 'D4', 'E3', 'E4']
     # k2 = ['D1', 'C2', 'B2', 'B3', 'B4', 'C5', 'D5', 'E5', 'F4', 'F3', 'F2', 'E2']
+    # k2 = ['D1', 'C2', 'B2', 'B3', 'B4', 'C5', 'D5', 'E5', 'F4', 'F3', 'F2', 'E2']
     # r1 = [k for k in dist_dic[1]]
     # r2 = [k for k in dist_dic[2]]
     # if sorted(r1) != sorted(k1):
@@ -85,8 +86,8 @@ def hex_dist(sp, ep):
     #     print('Expc:', sorted(k2))
     #     print('Test:', sorted(r2))
 
-    for i in dist_dic.items():
-        print(i)
+    # for i in dist_dic.items():
+    #     print(i)
     r = 0
     for i in dist_dic.keys():
         if ep in dist_dic[i]:
@@ -141,18 +142,16 @@ def find_enemy(you, dir, enemy):
 
 
 if __name__ == '__main__':
-    hex_dist("D3", "A1")
-    find_enemy("D3", "NE", "A1")
-    # assert find_enemy("D3", "NE", "A1") == ['L', 4], "Ext2"
-    # assert find_enemy("C3", "SE", "A1") == ['B', 3], "T"
-    # assert find_enemy('B2', 'S', 'B4') == ['F', 2], "T"
-    # assert find_enemy('G5', 'N', 'G4') == ['F', 1], "N-1"
-    # assert find_enemy('G5', 'N', 'I4') == ['R', 2], "NE-2"
-    # assert find_enemy('G5', 'N', 'J6') == ['R', 3], "SE-3"
-    # assert find_enemy('G5', 'N', 'G9') == ['B', 4], "S-4"
-    # assert find_enemy('G5', 'N', 'B7') == ['L', 5], "SW-5"
-    # assert find_enemy('G5', 'N', 'A2') == ['L', 6], "NW-6"
-    # assert find_enemy('G3', 'NE', 'C5') == ['B', 4], "[watch your six!]"
-    # assert find_enemy('H3', 'SW', 'E2') == ['R', 3], "right"
-    # assert find_enemy('A4', 'S', 'M4') == ['L', 12], "true left"
+    assert find_enemy("C3", "SE", "A1") == ['B', 3], "T"
+    assert find_enemy('B2', 'S', 'B4') == ['F', 2], "T"
+    assert find_enemy('G5', 'N', 'G4') == ['F', 1], "N-1"
+    assert find_enemy('G5', 'N', 'I4') == ['R', 2], "NE-2"
+    assert find_enemy('G5', 'N', 'J6') == ['R', 3], "SE-3"
+    assert find_enemy('G5', 'N', 'G9') == ['B', 4], "S-4"
+    assert find_enemy('G5', 'N', 'B7') == ['L', 5], "SW-5"
+    assert find_enemy('G5', 'N', 'A2') == ['L', 6], "NW-6"
+    assert find_enemy('G3', 'NE', 'C5') == ['B', 4], "[watch your six!]"
+    assert find_enemy('H3', 'SW', 'E2') == ['R', 3], "right"
+    assert find_enemy('A4', 'S', 'M4') == ['L', 12], "true left"
+    assert find_enemy("D3", "NE", "A1") == ['L', 4], "Ext2"
     print("You are good to go!")
